0016e2bb9592d63bfae9966f4cf7a7b523e3f150
Austin Lee <i@imnerd.org>
2023-03-19 10:28:24 +0800

    chore: add mongodb env adapter

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const url = require('url');
+const { MONGODB_URI } = process.env;
+if (MONGODB_URI) {
+  const connectUrl = url.parse(MONGODB_URI);
+  const [user, password] = connectUrl.auth.split(':');
+  process.env.MONGO_HOST = process.env.MONGO_HOST || connectUrl.hostname;
+  process.env.MONGO_PORT = process.env.MONGO_PORT || connectUrl.port;
+  process.env.MONGO_USER = process.env.MONGO_USER || user;
+  process.env.MONGO_PASSWORD = process.env.MONGO_PASSWORD || password;
+  process.env.MONGO_AUTHSOURCE = process.env.MONGO_AUTHSOURCE || 'admin';
+  process.env.MONGO_DB = process.env.MONGO_DB || 'waline';
+}
+
 const Waline = require('@waline/vercel');
 
 const app = Waline({
